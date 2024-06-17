e2873ad5771d1b2e02d7bf3e12d20549758eb22a
Mike Babb <mike7400@gmail.com>
2024-06-17 02:24:24 -0400

    removed dist

--- node-red-scripts/dist/get-domain-entities/filter-hidden.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const payload = msg.payload;
-const domains = flow.get("domains");
-const entities = payload.filter((entity) => entity.hidden_by === null).filter((entity) => {
-  const domain = entity.entity_id.split(".")[0];
-  return domains.includes(domain);
-});
-msg.payload = entities;
-return msg;
--- node-red-scripts/dist/get-domain-entities/reconcile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const entities = flow.get("entities");
-const areaEntities = flow.get("areaEntities");
-const areaEntitiesIds = areaEntities.map((areaEntity) => areaEntity.entity).flat();
-const areaEntitiesInArea = entities.filter(
-  (entity) => areaEntitiesIds.includes(entity.entity_id)
-);
-msg.payload = areaEntitiesInArea;
-return msg;
